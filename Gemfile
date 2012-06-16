e0458adaabf6bccd412847890379fab0d94c1be7
Sambalero <kevinswallow2@hotmail.com>
2012-06-16 16:38:36 -0700

    added datamapper

--- Gemfile
+++ Gemfile
@@ -2,8 +2,15 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'haml'
+gem 'datamapper'
 
 
 group :development do
   gem 'sinatra-reloader', require: 'sinatra/reloader'
+  gem 'sqlite3'
+  gem 'dm-sqlite-adapter'
 end     
+
+group :production do
+  gem 'dm-postgres-adapter'
+end
--- noobs.rb
+++ noobs.rb
@@ -1,9 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'data_mapper'
 require "sinatra/reloader" if development?  
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Comment
+  include DataMapper::Resource
+  property :id,           Serial
+  property :contact,      String, :required => true
+  property :verbiage,      String, :required => true
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do
   haml :welcome
+end
+
+get '/*' do
+  haml :not_found
 end
