6246af6ab6b8379514d66436de7390ab370514a3
Sambalero <kevinswallow2@hotmail.com>
2012-06-16 07:51:19 -0700

    improved formatting, haml

--- noobs.rb
+++ noobs.rb
@@ -1,29 +1,8 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 
 get '/' do
-  "This is the first deployment of the Noobs project app.
- The next deployment will feature
-    a description of the Noobs project
-    and (maybe) a form for getting public commentary."
+  haml :welcome
 end
-
-get '/:name' do
- "YO!"
-
-	@name = params[:name]
-  @time = Time.now.to_s
-	erb :hello
-end
-
-__END__
-@@ layout
-<html>
-  <body>
-   <%= yield %>
-  </body>
-</html>
-
-@@ hello
-<h3>Hello <%= @name %>! it's <%= @time %> at the server!</h3>
