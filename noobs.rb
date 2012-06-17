5fbeb7d01bac9765c93c59729df9bd44e13afbf7
Sambalero <kevinswallow2@hotmail.com>
2012-06-17 14:46:18 -0700

    replace pages after revert

--- noobs.rb
+++ noobs.rb
@@ -7,3 +7,19 @@ require "sinatra/reloader" if development?
 get '/' do
   haml :welcome
 end
+
+get '/commentary' do
+  haml :commentary
+end
+
+get '/page2' do
+  haml :page2
+end
+
+get '/*' do
+  haml :not_found
+end
+
+post '/' do
+  haml :next
+end    
