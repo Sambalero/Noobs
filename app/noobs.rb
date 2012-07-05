a23bb3c3d0e8ed559040a2545acfb007fa0fa817
Sambalero <kevinswallow2@hotmail.com>
2012-07-05 13:44:07 -0700

    put routes back in main

--- app/noobs.rb
+++ app/noobs.rb
@@ -10,7 +10,7 @@ require './db/models'  #necessary?
 # require controller files
 __DIR__ = ::File.dirname(__FILE__)
 require __DIR__ + '/controllers/init'
-require __DIR__ + '/../config/routes'
+
 require 'uri'
 
 
@@ -28,4 +28,44 @@ ActiveRecord::Base.establish_connection(
 )
 
 
+get '/' do
+  puts "In Main"
+  erb :welcome
+end
 
+get '/commentary' do
+  erb :commentary
+end
+
+get '/page2' do
+  erb :page2
+end
+
+get '/admin' do
+  erb :admin
+end
+
+get '/*' do
+  erb :not_found
+end
+
+post '/' do   
+
+  Pony.mail(
+    :from => "Noobs",
+    :to => 'kpswallow2@gmail.com',
+    :subject => params[:name],
+    :body => "comment: " + params[:comment] +"\n from: " + params[:contact],
+    :port => '587',
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+    :domain => 'swallowsnest.net',
+    :user_name => 'app5287164@heroku.com',
+    :password => '2tc2jutb',
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    })
+  erb :thankYou     
+end  
